ba3193a73a9582e22367d652890b3d2d812b52a8
jlund <jlund@binaryteaparty.com>
2022-05-17 11:35:44 -0600

    Improve Slack notification formatting.
    Unfurling enables link previews for the security content documents.

--- lib/slack.rb
+++ lib/slack.rb
@@ -2,7 +2,9 @@ module UpdateNotifier
   class Slack
     def self.notify(notification_message, config)
       request_headers = { "Content-Type" => "application/json" }
-      request_body    = { text: notification_message }
+      request_body    = { text: notification_message,
+                          unfurl_links: true,
+                          unfurl_media: true }
 
       config['webhook_urls'].each do |url|
         HTTParty.post(url, headers: request_headers, body: request_body.to_json)
