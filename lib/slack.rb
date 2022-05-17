1b21308bbf59643bc24013a8a63f35457d9ff85a
jlund <jlund@binaryteaparty.com>
2022-05-17 10:36:19 -0600

    Send JSON Content-Type header in Slack notifications.

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,10 +1,11 @@
 module UpdateNotifier
   class Slack
     def self.notify(notification_message, config)
-      request_body = { text: notification_message }
+      request_headers = { "Content-Type"  => "application/json" }
+      request_body    = { text: notification_message }
 
       config['webhook_urls'].each do |url|
-        HTTParty.post(url, body: request_body.to_json)
+        HTTParty.post(url, headers: request_headers, body: request_body.to_json)
       end
     end
   end
